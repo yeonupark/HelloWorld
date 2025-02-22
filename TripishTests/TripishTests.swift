f7e0b3b237a1e5c65f6c813622feec4c6639bad5
yeonupark <idepix@naver.com>
2025-02-22 23:03:38 +0900

    Rename project as Tripish

--- TripishTests/TripishTests.swift
+++ TripishTests/TripishTests.swift
@@ -6,9 +6,9 @@
 //
 
 import XCTest
-@testable import HelloWorld
+@testable import Tripish
 
-final class HelloWorldTests: XCTestCase {
+final class TripishTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
--- TripishUITests/TripishUITestsLaunchTests.swift
+++ TripishUITests/TripishUITestsLaunchTests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-final class HelloWorldUITestsLaunchTests: XCTestCase {
+final class TripishUITestsLaunchTests: XCTestCase {
 
     override class var runsForEachTargetApplicationUIConfiguration: Bool {
         true
